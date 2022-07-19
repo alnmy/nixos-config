d0cde5f3ad1ba1e216fb82b74800a35dc66fdc91
alan <alan@alnn.xyz>
2022-07-19 17:30:46 +0100

    secrets: Add lithium-desktop ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
 	users = [ alan ];
 	
 	pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOIZ5Y+7kK3Un4vMMKRphY9JzRUhUhMSxkhqq7J5a4l root@nixos";
+	lithium-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXfkLOdpHx7QvCjpmwuJo21OJJD1C3ylhgSSZhenKtU root@lithium-desktop";
 	systems = [ pi ];
 in
 {

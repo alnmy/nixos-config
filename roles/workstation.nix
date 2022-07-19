2545e6c4bb565ad9b01021c51788be63751e2ffc
alan <alan@alnn.xyz>
2022-07-19 09:33:09 +0000

    Enable xserver

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -40,6 +40,7 @@
 	networking.firewall.enable = true;
 
 	# Desktop
+	services.xserver.enable = true;
 	services.xserver.desktopManager.plasma5.enable = true;	
 	services.xserver.displayManager.sddm.enable = true;
 

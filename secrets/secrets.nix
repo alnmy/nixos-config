79d38cbd045dd0214fedc2b11168bc7217feb476
alan <alan@alnn.xyz>
2022-07-22 15:54:01 +0100

    secrets: add lithium-desktop to systems

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,9 +4,9 @@ let
 	
 	pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOIZ5Y+7kK3Un4vMMKRphY9JzRUhUhMSxkhqq7J5a4l root@nixos";
 	lithium-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIXfkLOdpHx7QvCjpmwuJo21OJJD1C3ylhgSSZhenKtU root@lithium-desktop";
-	systems = [ pi ];
+	systems = [ pi lithium-desktop ];
 in
 {
 	# TODO: actually use agenix, and use more than just my public key for the secret files.
-	"secret1.age".publicKeys = [ alan ];
+	"ddclient.age".publicKeys = alan ++ pi;
 }

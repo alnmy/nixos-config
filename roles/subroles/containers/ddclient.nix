a242f2c6f0daa25dca354bcce5159df1375f9279
alan <alan@alnn.xyz>
2022-07-26 16:29:45 +0100

    ddclient: add full config

--- roles/subroles/containers/ddclient.nix
+++ roles/subroles/containers/ddclient.nix
@@ -1,6 +1,24 @@
-{ config, pkgs, ... }: {
-	system.stateVersion = "22.05";
-	
-	services.ddclient.enable = true;
-	services.ddclient.configFile = "/etc/ddclient.conf";
+{ pkgs, ... }:
+{
+	age.secrets.ddclient = {
+		file = ../../../secrets/ddclient.nix;
+		path = "/etc/ddclient.conf";
+	};
+
+	containers.ddclient = {
+		config = { config, pkgs, ... }: {
+			system.stateVersion = "22.05";
+
+			services.ddclient.enable = true;
+			services.ddclient.configFile = "/etc/ddclient.conf";
+		};
+		autoStart = true;
+		privateNetwork = false;
+		bindMounts = {
+			"/etc/ddclient.conf" = {
+				hostPath = "/etc/ddclient.conf";
+				isReadOnly = true;
+			};
+		};
+	};
 }

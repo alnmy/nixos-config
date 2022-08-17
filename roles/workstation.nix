560a2bbe03b892f63896228a60b157d9086b0537
alan <alan@alnn.xyz>
2022-08-17 12:09:52 +0100

    workstation+firewall: Open ports for KDE-Connect

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -39,6 +39,11 @@
 
 	# Firewall
 	networking.firewall.enable = true;
+	# Open ports for KDE-Connect
+	networking.firewall = {
+		allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+		allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+	};
 
 	# Desktop
 	services.xserver.enable = true;

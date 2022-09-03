a813fea0675dfde0c5679dc7efc8e581234101a5
alan <alan@alnn.xyz>
2022-09-03 13:37:55 +0100

    VMWare: add expected semi-colon

--- roles/subroles/vmware.nix
+++ roles/subroles/vmware.nix
@@ -4,7 +4,7 @@
 		(self: super: {
 			vmware-workstation = super.vmware-workstation.overrideAttrs (oldAttrs: rec {
 				installPhase = oldAttrs.installPhase ++ "\nrm $out/lib/vmware/libconf/etc/fonts/fonts.conf";
-			})
+			});
 		})
 	];
 

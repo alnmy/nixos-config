d651989b8a4aebdc287ee9d9912d21081488b716
alan <alan@alnn.xzy>
2022-11-27 19:04:41 +0000

    VMWARE: Disable overlay

--- roles/subroles/vmware.nix
+++ roles/subroles/vmware.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
 {
-	nixpkgs.overlays = [
-		(self: super: {
-			vmware-workstation = super.vmware-workstation.overrideAttrs (oldAttrs: rec {
-				installPhase = (oldAttrs.installPhase) + ''
-					rm $out/lib/vmware/libconf/etc/fonts/fonts.conf
-				'';
-			});
-		})
-	];
-
 	virtualisation.vmware.host.enable = true;
 }
